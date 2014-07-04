d2e6a9bfd65586225c2edfae2d4f0a481f78f3d9
Arjun Raj <arraj@adobe.com>
2014-07-04 20:04:19 +0530

    Initial Checkin

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name awsvuiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the awsvuiApp
+ */
+angular.module('awsvuiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name awsvuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the awsvuiApp
+ */
+angular.module('awsvuiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
